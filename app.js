407f0d5300e7e636d8073b5ed6991a1fbec20a00
SubsJoneZ <58401439+SubsJoneZ@users.noreply.github.com>
2022-12-26 14:25:16 -0800

    new commit with product sample page at 50 percent

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const wrapper = document.querySelector(".sliderWrapper");
 const menuItems = document.querySelectorAll(".menuItem");
 
-menuItems.forEach
- console.log ("hello this is js!")  
+menuItems.forEach((item,index)=>{
+    item.addEventListener("click", ()=>{
+        wrapper.style.transform= `translateX(${-100 * index}vw)`; 
+    });
+});
+  
